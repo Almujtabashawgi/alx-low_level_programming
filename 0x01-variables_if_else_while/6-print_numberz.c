4e0b0fd66b9e8569fa44dc2105a3f326d4210002
almujtaba <almujtabashawgi@gmail.com>
2023-08-12 07:53:02 -0700

    aaa

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all alphabet
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int diit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
