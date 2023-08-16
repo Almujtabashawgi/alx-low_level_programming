6643dd12e65878e175050e63c8624f16da75047f
almujtaba <almujtabashawgi@gmail.com>
2023-08-16 03:09:14 -0700

    a

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,11 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
+ *
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Return: largest number
  */
 
@@ -24,5 +26,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
