ef3745b4f3ea1afc0bc23d0e5afb8369a7907495
almujtaba <almujtabashawgi@gmail.com>
2023-08-17 02:36:54 -0700

    a

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers
+ *
+ * Return: 0 Always
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num >= 0 && num <= 9)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
+
