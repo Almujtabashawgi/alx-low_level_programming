b919e28af7c662364a94fd7c5d768dcb0fa8241b
almujtaba <almujtabashawgi@gmail.com>
2023-08-21 08:15:24 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: the string
+ */
+
+void _puts(char *str)
+{
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	_puts(str);
+	_puts("\n");
+}
+
