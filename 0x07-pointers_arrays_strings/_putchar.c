7b0639289f29e081779fc57dc74ee66f0d62a0c5
almujtaba <almujtabashawgi@gmail.com>
2023-08-28 14:01:59 -0700

    a

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - Writes the char c to std
+ * @c: the charator to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: on success 1.
+ *         on error, -1 is returend,
  */
 int _putchar(char c)
 {
