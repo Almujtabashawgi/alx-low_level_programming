194671c46ecd52707bc96c89a270eb5aa075b294
almujtaba <almujtabashawgi@gmail.com>
2023-08-28 13:53:14 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,31 @@
-#include "main.h"
+#include "holberton.h"
+#include "stdio.h"
+
 /**
- * print_chessboard - function that prints the chessboard.
- *@a: first value -char
+ * print_diagsums- sum diagonal values
+ *
+ * @a: pointer to array
+ *
+ * @size: int
+ *
+ *Return: sum of two sides
  *
  */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
 
-	for (i = 0; i < 8; i++)
+void print_diagsums(int *a, int size)
+{
+	int counter1, counter2, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (counter1 = 0; counter1 < size; counter1++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar (a[i][j]);
-		}
-		_putchar('\n');
+		sum1 += a[(size + 1) * counter1];
 	}
+	for (counter2 = 0; counter2 < size; counter2++)
+	{
+		sum2 += a[(size - 1) * (counter2 + 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
