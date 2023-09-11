a811c4da7759bdd4d761ab2322ba40e468bd11a3
almujtaba <almujtabashawgi@gmail.com>
2023-09-11 08:27:20 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - a strcture function
- * @name: a char var
- * @age: a float var
- * @owner: a char var
- * Description: A struct dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
 struct dog
 {
-	char name;
+	char *name;
 	float age;
-	char owner;
+	char *owner;
 };
 
 /**
