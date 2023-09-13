1076e12a97cd2436fe8e4cc3c1a22ee7d6045953
almujtaba <almujtabashawgi@gmail.com>
2023-09-13 02:34:50 -0700

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: the function call
  * Return: Nothing.
  */
 
-void print_name_as_is(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
